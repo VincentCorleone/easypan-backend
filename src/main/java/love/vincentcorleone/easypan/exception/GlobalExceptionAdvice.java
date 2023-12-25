b003b8e58d2dcb7199f2cd1bb99a73661921e7ad
VincentCorleone <707119577@qq.com>
2023-12-25 19:54:00 +0800

    debug for white error page(2)

--- src/main/java/love/vincentcorleone/easypan/exception/GlobalExceptionAdvice.java
+++ src/main/java/love/vincentcorleone/easypan/exception/GlobalExceptionAdvice.java
@@ -2,18 +2,22 @@ package love.vincentcorleone.easypan.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionAdvice {
 
     @ExceptionHandler({RuntimeException.class})
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseResult<Object> handleException(HttpServletRequest request, Exception e){
         return ResponseResult.fail(e.getMessage());
     }
 
     @ExceptionHandler({ConstraintViolationException.class})
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseResult<Object> handleException(HttpServletRequest request, ConstraintViolationException e){
         return ResponseResult.fail(e.getMessage());
     }
